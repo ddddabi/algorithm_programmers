b1833910ef0da142fd50bff9f02273cd956ffd5d
dabin Jung <88383179+ddddabi@users.noreply.github.com>
2026-01-28 22:01:08 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.02 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String s) {
+        int len = s.length();
+
+        // 홀수
+        if (len % 2 == 1) {
+            return s.substring(len / 2, len / 2 + 1);
+        }
+        // 짝수
+        else {
+            return s.substring(len / 2 - 1, len / 2 + 1);
+        }
+    }
+}
